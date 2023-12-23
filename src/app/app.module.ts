64c9042fcf357a7088f986e3c87a5ed96b0a44d4
Prasoon <Prasoonbhargava71694@gmail.com>
2023-12-23 23:05:48 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { CountryListComponent } from './country-list/country-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserlistComponent,
+    UserListComponent,
+    StudentListComponent,
+    CountryListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
